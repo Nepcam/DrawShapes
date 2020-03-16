38ef07590141fb6cca39310e43aa8fd7749eb5ca
Nepcam <cameron.nepe@gmail.com>
2020-03-16 21:24:03 +1300

    created buttons, gave them functions to draw a box with different pens and colors

--- DrawShapes/Form1.Designer.cs
+++ DrawShapes/Form1.Designer.cs
@@ -30,36 +30,68 @@
         {
             this.pictureBoxDisplay = new System.Windows.Forms.PictureBox();
             this.buttonDrawLine = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonDrawSquare = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBoxDisplay
             // 
+            this.pictureBoxDisplay.BackColor = System.Drawing.SystemColors.ControlDark;
             this.pictureBoxDisplay.Location = new System.Drawing.Point(0, 0);
             this.pictureBoxDisplay.Name = "pictureBoxDisplay";
-            this.pictureBoxDisplay.Size = new System.Drawing.Size(799, 384);
+            this.pictureBoxDisplay.Size = new System.Drawing.Size(799, 372);
             this.pictureBoxDisplay.TabIndex = 0;
             this.pictureBoxDisplay.TabStop = false;
             // 
             // buttonDrawLine
             // 
+            this.buttonDrawLine.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.buttonDrawLine.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonDrawLine.Location = new System.Drawing.Point(319, 388);
+            this.buttonDrawLine.Location = new System.Drawing.Point(12, 390);
             this.buttonDrawLine.Name = "buttonDrawLine";
             this.buttonDrawLine.Size = new System.Drawing.Size(180, 48);
             this.buttonDrawLine.TabIndex = 1;
             this.buttonDrawLine.Text = "Draw Line";
-            this.buttonDrawLine.UseVisualStyleBackColor = true;
+            this.buttonDrawLine.UseVisualStyleBackColor = false;
+            this.buttonDrawLine.Click += new System.EventHandler(this.buttonDrawLine_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.BackColor = System.Drawing.Color.Red;
+            this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExit.Location = new System.Drawing.Point(608, 390);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(180, 48);
+            this.buttonExit.TabIndex = 2;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = false;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // buttonDrawSquare
+            // 
+            this.buttonDrawSquare.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.buttonDrawSquare.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDrawSquare.Location = new System.Drawing.Point(310, 390);
+            this.buttonDrawSquare.Name = "buttonDrawSquare";
+            this.buttonDrawSquare.Size = new System.Drawing.Size(180, 48);
+            this.buttonDrawSquare.TabIndex = 3;
+            this.buttonDrawSquare.Text = "Draw Square";
+            this.buttonDrawSquare.UseVisualStyleBackColor = false;
+            this.buttonDrawSquare.Click += new System.EventHandler(this.buttonDrawSquare_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonDrawSquare);
+            this.Controls.Add(this.buttonExit);
             this.Controls.Add(this.buttonDrawLine);
             this.Controls.Add(this.pictureBoxDisplay);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Drawing Application";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).EndInit();
             this.ResumeLayout(false);
 
@@ -69,6 +101,8 @@
 
         private System.Windows.Forms.PictureBox pictureBoxDisplay;
         private System.Windows.Forms.Button buttonDrawLine;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonDrawSquare;
     }
 }
 
--- DrawShapes/Form1.cs
+++ DrawShapes/Form1.cs
@@ -16,5 +16,35 @@ namespace DrawShapes
         {
             InitializeComponent();
         }
+
+        private void buttonDrawLine_Click(object sender, EventArgs e)
+        {
+            Graphics paper = pictureBoxDisplay.CreateGraphics();
+            Pen pen1 = new Pen(Color.Blue, 5);
+            Pen pen2 = new Pen(Color.Red, 5);
+
+            paper.DrawLine(pen1, 10, 10, 100, 100);
+            paper.DrawLine(pen2, 200, 200, 400, 400);
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonDrawSquare_Click(object sender, EventArgs e)
+        {
+            Graphics paper = pictureBoxDisplay.CreateGraphics();
+            Pen pen1 = new Pen(Color.Blue, 5);
+            Pen pen2 = new Pen(Color.Red, 5);
+            Pen pen3 = new Pen(Color.Green, 5);
+            Pen pen4 = new Pen(Color.Black, 5);
+
+            paper.DrawLine(pen1, 10, 10, 10, 100);
+            paper.DrawLine(pen2, 10, 100, 100, 100);
+            paper.DrawLine(pen3, 100, 100, 100, 10);
+            paper.DrawLine(pen4, 100, 10, 10, 10);
+
+        }
     }
 }
