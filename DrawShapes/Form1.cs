b8d908ba3d7b6113574aac1232b48eff153ee642
Nepcam <cameron.nepe@gmail.com>
2020-03-19 14:53:13 +1300

    updated for practical assignment

--- DrawShapes/Form1.cs
+++ DrawShapes/Form1.cs
@@ -17,37 +17,33 @@ namespace DrawShapes
             InitializeComponent();
         }
 
+        // Exercise 1a
         private void buttonDrawLine_Click(object sender, EventArgs e)
         {
-            // Creates the Graphics object and links it to the pictureBoxDisplay
-            Graphics paper = pictureBoxDisplay.CreateGraphics();
-            // Created Pen objects
+            Graphics canvas = pictureBoxDisplay.CreateGraphics();
             Pen bluePen = new Pen(Color.Blue, 5);
-            Pen redPen = new Pen(Color.Red, 5);
-
-            // Created a DrawLine object and combined the paper and Pen objects
-            paper.DrawLine(bluePen, 10, 10, 100, 100);
-            paper.DrawLine(redPen, 200, 200, 400, 400);
+            canvas.DrawLine(bluePen, 10, 10, 100, 100);
         }
-        // Closes the application 
+
+        // Exercise 1b 
         private void buttonExit_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
+        // Exercise 1c
         private void buttonDrawSquare_Click(object sender, EventArgs e)
         {
-            Graphics paper = pictureBoxDisplay.CreateGraphics();
+            Graphics canvas = pictureBoxDisplay.CreateGraphics();
             Pen bluePen = new Pen(Color.Blue, 5);
             Pen redPen = new Pen(Color.Red, 5);
             Pen greenPen = new Pen(Color.Green, 5);
             Pen blackPen = new Pen(Color.Black, 5);
 
-            paper.DrawLine(bluePen, 10, 10, 10, 100);
-            paper.DrawLine(redPen, 10, 100, 100, 100);
-            paper.DrawLine(greenPen, 100, 100, 100, 10);
-            paper.DrawLine(blackPen, 100, 10, 10, 10);
-
+            canvas.DrawLine(bluePen, 10, 10, 10, 100);
+            canvas.DrawLine(redPen, 10, 100, 100, 100);
+            canvas.DrawLine(greenPen, 100, 100, 100, 10);
+            canvas.DrawLine(blackPen, 100, 10, 10, 10);
         }
     }
 }
